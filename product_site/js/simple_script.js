dc72f2423c183b98c31adde593b04bbbf6674706
nroy0703 <nroy0703@uri.edu>
2024-11-16 21:11:05 -0800

    Added event listeners
    Added event listeners for assignment 11

--- product_site/js/simple_script.js
+++ product_site/js/simple_script.js
@@ -40,7 +40,7 @@ form_style.style.color = "white";
 
 //Using arithmetic operators to calculate the stock quantity of a certain device
 
-//Function that takes in two parameters, the starting number stock and the number of devices sold, and subracts the two numbers to reveal a message alerting the user of the low stock quantity
+//Function that takes in two parameters, the starting number stock and the number of devices sold, and subtracts the two numbers to reveal a message alerting the user of the low stock quantity
 function stock_message(initial_stock_quantity, devices_sold) {
     //Using arithmetic operators to calculate the stock quantity of a certain device
     //Variable that stores the number of devices that are left in stock
@@ -49,8 +49,56 @@ function stock_message(initial_stock_quantity, devices_sold) {
     //Get the id in my customer_tech_support.html file and store it in variable elStock
     var elStock = document.getElementById('low-stock-notice');
     //Use innerHTML to add a message to the elStock variable that alerts users of the low stock quantity for Amazon Fire tablets
-    elStock.innerHTML = '<strong> Important Notice! </strong>Only ' + stock_left + ' Amazon Fire tablets left in stock!';
+    elStock.innerHTML = 'Only ' + stock_left + ' Amazon Fire tablets left in stock!';
 }
 
-//Call the function stock_message with the values 50 and 30
-stock_message(50, 30);
+//Variable that stores a boolean value that determines whether the message is displayed or not
+var isMessageDisplayed = false;
+//Add an event listener to the button with id 'low-stock-notice' to call the function stock_message when the button is clicked
+document.getElementById('low-stock-notice').addEventListener('click', function() {
+   
+    //Variable message to get the id again in order to change the message
+    var message = document.getElementById('low-stock-notice');
+    //When the button is clicked initally, the message in the stcok_message function is displayed
+    if(isMessageDisplayed === false){
+        stock_message(50, 30);
+        isMessageDisplayed = true;
+    }
+    //Once the user clicks the button again, the message goes back to the original message
+    else{
+        message.innerHTML = '<strong>Important Notice! Click here</strong>';
+        isMessageDisplayed = false;
+    }
+});
+
+//Form Validation
+
+//Get the form class in my customer_tech_support.html file and store it in variable getForm
+var getForm = document.querySelector('.form');
+//Add an event listener to the form to call the function when the form is submitted
+getForm.addEventListener('submit', function(event) {
+    //Get the device input class in customer_tech_support.html and store it in a variable
+    var deviceTypeSelected = document.querySelector('input[class="device"]:checked');
+    //Get the price input class in customer_tech_support.html and store it in a variable
+    var priceRangeSelected = document.querySelector('input[class="range"]:checked');
+    //Get the use case input class in customer_tech_support.html and store it in a variable
+    var useCaseSelected = document.querySelectorAll('input[class="use-case"]:checked');
+    //Get the message id in customer_tech_support.html and store it in variable errorMessage, that will be used to display the error message
+    var errorMessage = document.querySelector('#message');
+
+    //Set the text content of the errorMessage to an empty string
+    errorMessage.textContent = '';
+    
+    //If statement that checks if the deviceTypeSelected, priceRangeSelected, and useCaseSelected are not selected, then display an error message
+    if (!deviceTypeSelected || !priceRangeSelected || useCaseSelected.length === 0) {
+        event.preventDefault();
+        errorMessage.textContent = 'Submission Unsuccessful. Please select a device, price range, and at least one purpose.';
+        errorMessage.style.color = 'red';
+    //Else statement that alerts the user that the form was submitted successfully
+    } else {
+        event.preventDefault();
+        errorMessage.textContent = '';
+        alert('Form submitted successfully!');
+    }
+});
+
